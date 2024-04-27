834ed3ef7d7b2f866a35f6568a31328e57f1c601
coesxtt <jv9472@gmail.com>
2024-04-26 22:36:37 -0300

    add funçoes da API no projeto

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,22 @@
 function consultaCep(){ //criando função para consultar API
 
+    let campo = document.querySelector("#cep")
     let requisicao = new XMLHttpRequest(); // definindo os prarametros da requisição
-    let url = "https://viacep.com.br/ws/17054267/json/"; // colocando o link da api em JSON
+    let url = "https://viacep.com.br/ws/" + campo.value + "/json/";
     requisicao.open("GET", url); // primeiramente fazendo a abertura do url
     requisicao.send(); // envia a requisição para a API
     requisicao.onload = function(){ // retorno quando a função foi executada com sucesso
-        alert(JSON.parse(requisicao.response).logradouro)
+        let objetoCEP = JSON.parse(requisicao.response)
+        let campoLocalidade = document.querySelector("#localidade")
+        let campoUF = document.querySelector("#uf")
+        let campoEndereco = document.querySelector("#logradouro")
+        let campoComplemento = document.querySelector("#complemento")
+
+        campoLocalidade.value = objetoCEP.localidade 
+        campoUF.value = objetoCEP.uf
+        campoEndereco.value = objetoCEP.logradouro
+        campoComplemento.value = objetoCEP.complemento
+        //alert(objetoCEP.localidade + "-" + objetoCEP.uf + ", " + objetoCEP.logradouro + " " + objetoCEP.complemento)
     }
 }
 
